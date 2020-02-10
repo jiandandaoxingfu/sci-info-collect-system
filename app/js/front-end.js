b5e014e05f68600c17eec0dae805a0f0b2eecd0f
jiandandaoxingfu <jiaminxin@outlook.com>
2020-02-10 16:32:29 +0800

    Signed-off-by: jiandandaoxingfu <jiaminxin@outlook.com>

--- app/js/front-end.js
+++ app/js/front-end.js
@@ -16,6 +16,7 @@ class App {
 	input_valid_check() {
 		let title_arr = document.getElementById('title').value.replace(/，/g, ',').split(',').map(d => d.replace(/(^\s*)/, ''));
 		let author = document.getElementById('author').value;
+		let threads = parseInt(document.getElementById('threads').value);
 		let year = document.getElementById('year').value.replace(/，/g, ',').replace(/\s/g, '').split(',');
 		if (title_arr[0] === "" || author === '' || year[0] === "") {
 			alert('请输入标题， 年份， 作者');
@@ -23,6 +24,7 @@ class App {
 		} else {
 			this.title_arr = title_arr;
 			this.year = year;
+			this.threads = threads;
 			this.cite_tabs_id = new Array(title_arr.length).join(',').split(',');
 			return this.name_format(author);
 		}
@@ -67,6 +69,12 @@ class App {
 	}
 
 	restart() {
+		this.cite_tabs_id.forEach( id => {
+			if( id  !== '' ) {
+				chrome.tabs.remove(id);
+			}
+		})
+		chrome.tabs.remove(this.spider_tab_id);
 		window.open(window.location.href, '_self');
 	}
 
@@ -75,7 +83,6 @@ class App {
 			let is_related_tabid = tabid == this.spider_tab_id ||
 											this.cite_tabs_id.indexOf(tabid) > -1;
 			if(this.is_start && is_related_tabid) {
-				console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '发送开始命令');
 				message.send(tabid, 'is-start', {info: ''});
 			}
 		})
@@ -84,14 +91,13 @@ class App {
 			if( !msg.info ) {
 				alert('无法获取web of science权限，请检查是否具有使用其权限。一般而言要使用校园网');
 			} else {
-				console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '已经获取sid')
 				message.send(tabid, 'init-data', {title_arr: this.title_arr, author_arr: this.author_arr, threads: this.threads});
 			}
 		})
 
 		message.on('save-spider', msg => {
 			this.spider = msg.spider;
-			console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '已保存spider');
+			console.log( '已保存spider' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		})
 
 		message.on('get-spider', (msg, tabid) => {
@@ -100,13 +106,13 @@ class App {
 
 		message.on('search-info', msg => {
 			if( msg.info === 'success' ) {
-				console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + `第${msg.id}个搜索成功`);
+				console.log( `${msg.id + 1}  搜索成功` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 			} else if( msg.info === 'not unique' ) {
-				console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + `第${msg.id}个搜索结果不唯一`);
+				console.log( `${msg.id + 1}  搜索结果不唯一` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 			} else if( msg.info === 'success & no cite' ) {
-				console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + `第${msg.id + 1}个搜索完成，0引用`);
-			} else {
-				console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + `第${msg.id}个搜索失败`);
+				console.log( `${msg.id + 1}  搜索完成，0引用` + new Date().getMinutes() + ':' + new Date().getSeconds() );
+			} else if( msg.info === 'not found' ){
+				console.log( `${msg.id + 1}  搜索失败` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 			}
 		});
 
@@ -116,7 +122,7 @@ class App {
 				url: msg.url
 			}, (tab) => {
 				this.cite_tabs_id[msg.id] = tab.id;
-				console.log(msg.id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '已经打开引用窗口');
+				console.log( msg.id + 1 + ' : ' + '已经打开引用窗口' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 			})
 		})
 
@@ -126,29 +132,32 @@ class App {
 
 			} else if( msg.info === 'no-2018-cite' ){
 				chrome.tabs.remove(tabid);
+				this.cite_tabs_id[this.cite_tabs_id.indexOf(tabid)] = '';
 			} else { // error
 				chrome.tabs.remove(tabid);
+				this.cite_tabs_id[this.cite_tabs_id.indexOf(tabid)] = '';
 			}
-			console.log(this.cite_tabs_id.indexOf(tabid) + ' : ' + msg.info);
+			console.log( this.cite_tabs_id.indexOf(tabid) + 1 + ' : ' + msg.info + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		});
 
 		message.on('cite-refine-info', (msg, tabid) => {
 			chrome.tabs.remove(tabid);
-			message.send(this.spider_tab_id, 'cite-refine-info', {id: this.cite_tabs_id.indexOf(tabid), data: msg.data});
+			this.cite_tabs_id[this.cite_tabs_id.indexOf(tabid)] = '';
+			message.send(this.spider_tab_id, 'cite-refine-info', {id: this.cite_tabs_id.indexOf(tabid), data: msg.data, info: msg.info});
 			if(msg.info) {
 				
 			} else { // error
 				
 			}
-			console.log(this.cite_tabs_id.indexOf(tabid) + ' : cite-refine-data' + msg.info);
+			console.log( this.cite_tabs_id.indexOf(tabid) + 1 + ' : cite-refine-data' + msg.info + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		});
 
 		message.on('single-done', msg => {
-			console.log(`第${msg.id + 1}个已经完成`)
+			console.log(  `${msg.id + 1} 完成` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		})
 
 		message.on('done', msg => {
-			console.log(`完成了`);
+			console.log( `完成了` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 			app.spider = msg.spider;
 		})
 	}
--- content-script/js/spider.js
+++ content-script/js/spider.js
@@ -1,23 +1,24 @@
 class Spider {
 	get_sid() {
-		console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '获取sid')
+		console.log('获取sid' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		let sid = window.location.href.match(/&SID=(.*?)&/);
 		if( sid ) {
 			this.sid = sid[1];
 			message.send('sid', {info: true});
-			console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '成功获取sid')
+			console.log('成功获取sid' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		} else {
 			message.send('sid', {info: false});
-			console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '无法获取sid')
+			console.log('无法获取sid' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		}
 	}
 
 	start() {
 		message.on('init-data', init_data => {
-			console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '已获取title & author arr');
+			console.log('已获取title & author arr' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 			this.init(init_data);
 			this.save_spider();
-			for(let i=0; i<this.threads; i++) {
+			let n = Math.min(this.threads, this.title_arr.length);
+			for(let i=0; i<n; i++) {
 				this.crawl(i);
 			}
 		})
@@ -30,7 +31,7 @@ class Spider {
 		spider.data_format_ = spider.data_format.toString();
 		spider.get_cite_num_ = spider.get_cite_num.toString();
 		message.send('save-spider', {spider: this});
-		console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '正在保存spider');
+		console.log('正在保存spider' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 	}
 
 	init(data) {
@@ -43,7 +44,7 @@ class Spider {
 		this.search_datas = new Array(n).join(',').split(',');
 		this.cite_refine_datas = new Array(n).join(',').split(',');
 		this.detail_tables = new Array(n).join(',').split(',');
-		this.search_states = new Array(n).join(',').split(',').map( e => [0, 0, 0, 0] ); // search, cite-refine, detail，journal.　0/1/2: undo/doing/done.
+		this.search_states = new Array(n).join(',').split(',').map( e => [0, 0, 0, 0, 0] ); // search, cite-refine, detail，journal: -1/0/1/2， error/undo/doing/done; is-done: 0/1，undo/done;
 		this.cite_num_arr = new Array(n).join(',').split(',').map( e => [0, 0] );
 		this.search_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com/WOS_GeneralSearch.do?fieldCount=1&action=search&product=WOS&search_mode=GeneralSearch&SID=${this.sid}&max_field_count=25&max_field_notice=%E6%B3%A8%E6%84%8F%3A+%E6%97%A0%E6%B3%95%E6%B7%BB%E5%8A%A0%E5%8F%A6%E4%B8%80%E5%AD%97%E6%AE%B5%E3%80%82&input_invalid_notice=%E6%A3%80%E7%B4%A2%E9%94%99%E8%AF%AF%3A+%E8%AF%B7%E8%BE%93%E5%85%A5%E6%A3%80%E7%B4%A2%E8%AF%8D%E3%80%82&exp_notice=%E6%A3%80%E7%B4%A2%E9%94%99%E8%AF%AF%3A+%E4%B8%93%E5%88%A9%E6%A3%80%E7%B4%A2%E8%AF%8D%E5%8F%AF%E4%BB%A5%E5%9C%A8%E5%A4%9A%E4%B8%AA%E5%AE%B6%E6%97%8F%E4%B8%AD%E6%89%BE%E5%88%B0+%28&input_invalid_notice_limits=+%3Cbr%2F%3E%E6%B3%A8%E6%84%8F%3A+%E6%BB%9A%E5%8A%A8%E6%A1%86%E4%B8%AD%E6%98%BE%E7%A4%BA%E7%9A%84%E5%AD%97%E6%AE%B5%E5%BF%85%E9%A1%BB%E8%87%B3%E5%B0%91%E4%B8%8E%E4%B8%80%E4%B8%AA%E5%85%B6%E4%BB%96%E6%A3%80%E7%B4%A2%E5%AD%97%E6%AE%B5%E7%9B%B8%E7%BB%84%E9%85%8D%E3%80%82&sa_params=WOS%7C%7C7AVrjhmEcJpyJsy2QBT%7Chttp%3A%2F%2Fapps.webofknowledge.com%7C%27&formUpdated=true&value%28input1%29=_title_&value%28select1%29=TI&value%28hidInput1%29=&limitStatus=collapsed&ss_lemmatization=On&ss_spellchecking=Suggest&SinceLastVisit_UTC=&SinceLastVisit_DATE=&period=Range+Selection&range=ALL&startYear=1985&endYear=2020&editions=SCI&editions=SSCI&editions=AHCI&editions=ISTP&editions=ESCI&editions=CCR&editions=IC&update_back2search_link_param=yes&ssStatus=display%3Anone&ss_showsuggestions=ON&ss_numDefaultGeneralSearchFields=1&ss_query_language=&rs_sort_by=PY.D%3BLD.D%3BSO.A%3BVL.D%3BPG.A%3BAU.A`; // sid, title
 		this.cite_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+CitingArticles.do?product=WOS&SID=${this.sid}&search_mode=CitingArticles&parentProduct=WOS&parentQid=_qid_&parentDoc=1&REFID=_refid_&logEventUT=WOS:000340351500004&excludeEventConfig=ExcludeIfFromNonInterProduct&cacheurlFromRightClick=no`; // sid, qid, refid,
@@ -53,47 +54,51 @@ class Spider {
 
 		message.on('cite-info', msg => {
 			if( msg.info !== 'has-2018-cite' ) {
-				this.search_states[msg.id][1] = 2;
-				this.search_states[msg.id][2] = 2;
-				this.search_states[msg.id][3] = 2;
+				this.search_states[msg.id][4] = 1;
+				if( msg.info === 'cite-page-error' ) {
+					this.search_states[msg.id][1] = -1;
+				}
 				message.send('single-done', {id: msg.id});
 				this.next();
 			} else {
+				this.search_states[msg.id][1] = 1;
 				this.get_detail_data(msg.id);
-				// this.get_journal_data(msg.id);
+				this.get_journal_data(msg.id);
 			}
 		})
 
 		message.on('cite-refine-info', msg => {
-			this.search_states[msg.id][1] = 2;
-			if( msg.info ) {
+			// if( msg.info ) {
+				this.search_states[msg.id][1] = 2;
 				this.cite_refine_datas[msg.id] = msg.data.data;
 				this.cite_num_arr[msg.id] = msg.data.cite_num;
-				console.log(msg.id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '已保存refine data');
-	
-				if(Math.min(...this.search_states[msg.id]) === 2) { // 这个标题完成了
+				console.log(msg.id + 1 + ' : ' + '已保存refine data' + new Date().getMinutes() + ':' + new Date().getSeconds() );
+				if( !this.search_states[msg.id].includes(1) ) { // 这个标题完成了
+					this.search_states[msg.id][4] = 1;
 					message.send('single-done', {id: msg.id});
 					this.next();
 				}
-			} else { // error
-				message.send('single-done', {id: msg.id});
-				this.next();
-			}
+			// } else { // error
+			// 	this.search_states[msg.id][1] = -1;
+			// 	this.search_states[msg.id][4] = 1;
+			// 	message.send('single-done', {id: msg.id});
+			// 	this.next();
+			// }
 		})
 		
-		console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + '已初始化');
+		console.log('已初始化' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 	}
 
 	get_search_data(id) {
-		console.log(id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '正在搜索');
+		console.log(id + 1 + ' : ' + '正在搜索' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.search_states[id][0] = 1;
 		let search_url = this.search_url.replace('_title_', this.title_arr[id]);
 		return axios.get(search_url).then( res => {
-			this.search_states[id][0] = 2;
 			let records = res.data.match(/id="RECORD_\d+"/g);
 			let info = '';
 			if( records ) {
 				if( records.length === 1 ) {
+					this.search_states[id][0] = 2;
 					info = 'success';
 					let data = res.data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/)[0];
 					this.journal_arr[id] = data.match(/<value>(.*?)<\/value>/)[1];
@@ -106,15 +111,15 @@ class Spider {
 					}
 				} else {
 					info = 'not unique';
+					this.search_states[msg.id][0] = -1;
 				}
 			} else {
 				info = 'not found';
+				this.search_states[msg.id][0] = -1;
 			}
 
 			if( info !== 'success' ) {
-				this.search_states[id][1] = 2;
-				this.search_states[id][2] = 2;
-				this.search_states[id][3] = 2;
+				this.search_states[id][4] = 1;
 				message.send('single-done', {id: id});
 				this.next();
 			}
@@ -124,25 +129,25 @@ class Spider {
 	}
 
 	get_journal_data(id) { 
-		// 根据期刊名称获取其中科院分区信息
-		console.log(id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '正在获取期刊的分区信息');
+		console.log(id + 1 + ' : ' + '抓取期刊分区页' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.search_states[id][3] = 1;
-		let journal_info_url = this.journal_info_url.replace('_journal_', journal);
-		return axios.get(journal_info_url).then( res => {
-			this.search_states[id][3] = 2;
-			if( true ) {
-				let data = res.data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/)[0];
-				this.journal_datas[id] = this.data_format(data);
-				console.log(id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '已经获取期刊的分区信息');
-			} else {
-
-			}
-		});
+		// let journal_url = this.journal_url.replace('_journal_', this.journal_arr[id]);
+		// return axios.get(journal_url).then( res => {
+		// 	if( true ) { // 判断是否成功。
+				this.search_states[id][3] = 2;
+		// 		this.detail_tables[id] = this.table_format(res.data);
+		// 	} else {
+		// 		this.search_states[id][3] = -1;
+		// 	}
+		// 	if( !this.search_states[id].includes(1) ) { // 这个标题完成了
+		// 		message.send('single-done', {id: id});
+		// 		this.next();
+		// 	}
+		// })
 	}
 
 	open_cite_page(id) {
-		console.log(id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '发送打开引用窗口');
-		this.search_states[id][1] = 1;
+		console.log(id + 1 + ' : ' + '发送打开引用窗口' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		let cite_url = this.cite_url.replace('_qid_', this.qid_arr[id])
 			.replace('_refid_', this.refid_arr[id]);
 		message.send('open-cite-page', {url: cite_url, id: id})
@@ -170,35 +175,35 @@ class Spider {
 	}
 
 	get_detail_data(id) {
-		console.log(id + ' : ' + new Date().getMinutes() + ':' + new Date().getSeconds() + '抓取详情页')
+		console.log(id + 1 + ' : ' + '抓取详情页' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.search_states[id][2] = 1;
 		let detail_url = this.detail_url.replace('_qid_', this.qid_arr[id])
 			.replace('_title_', this.title_arr[id]);
 		return axios.get(detail_url).then( res => {
-			this.search_states[id][2] = 2;
-			if( true ) {
+			if( true ) { // 判断是否成功。
+				this.search_states[id][2] = 2;
 				this.detail_tables[id] = this.table_format(res.data);
-
-				if(Math.min(...this.search_states[id]) === 2) { // 这个标题完成了
-					message.send('single-done', {id: id});
-					this.next();
-				}
 			} else {
-
+				this.search_states[id][2] = -1;
+			}
+			if( !this.search_states[id].includes(1) ) { // 这个标题完成了
+				message.send('single-done', {id: id});
+				this.next();
 			}
 		})
 	}
 
 	get_cite_refine_data(data) {
-		let records = data.match(/id="RECORD_\d+"/g);
-		if( records ) {
+		// 由于精炼页面时从引用页面自然进来，因此基本不会出错。
+		// let records = data.match(/id="RECORD_\d+"/g);
+		// if( records ) {
 			data = data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/)[0]
 			data = this.data_format(data);
 			data = this.get_cite_num(data);
 			message.send('cite-refine-info', {info: true, data: data});
-		} else {
-			message.send('cite-refine-info', {info: false, data: ''});
-		}
+		// } else {
+		// 	message.send('cite-refine-info', {info: false, data: ''});
+		// }
 	}
 
 	data_format(data) {
@@ -239,12 +244,17 @@ class Spider {
 
 	table_format(data) {
 		body.innerHTML = data;
-		data = body.querySelectorAll('table')[2];
-		body.innerHTML = '';
-		body.appendChild(data);
-		data.style.padding = '25px';
-		body.setAttribute('class', 'printWhitePage');
-		return body.innerHTML;
+		data = body.querySelectorAll('table');
+		if( data[2] ) {
+			body.innerHTML = '';
+			body.appendChild(data[2]);
+			data[2].style.padding = '25px';
+			body.setAttribute('class', 'printWhitePage');
+			return body.innerHTML;
+		} else {
+			message.send('')
+			return '';
+		}
 	}
 
 	get_cite_num(data) {
@@ -268,9 +278,9 @@ class Spider {
 	}
 
 	crawl(id) {
-		console.log(new Date().getMinutes() + ':' + new Date().getSeconds() + `第${id + 1}个开始运行`);
+		console.log(`${id + 1} : 个开始运行` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.get_search_data(id).then( () => {
-			if( this.search_states[id][0] == 2 && this.search_states[id][1] === 0 ) {
+			if( this.search_states[id][0] == 2 ) {
 				this.open_cite_page(id);
 			}
 		})
@@ -280,10 +290,10 @@ class Spider {
 		let n = this.search_states.length;
 		let finished_num = 0;
 		for(let i=0; i<n; i++) {
-			if( Math.max(...this.search_states[i]) === 0 ) {
+			if( this.search_states[i][0] === 0 ) {
 				this.crawl(i);
 				return
-			} else if( Math.min(...this.search_states[i]) === 2 ) {
+			} else if( this.search_states[i][4] === 1 ) {
 				finished_num += 1;
 			}
 		}
@@ -297,6 +307,7 @@ class Spider {
 		message.send('done', {spider: spider});
 		body.innerHTML = '';
 		for(let i=0; i<this.qid_arr.length; i++) {
+			body.innerHTML += `<h2>${i + 1} ： ${this.title_arr[i]}</h2>`;
 			body.innerHTML += this.search_datas[i];
 			body.innerHTML += this.cite_refine_datas[i];
 			body.innerHTML += this.detail_tables[i];
@@ -361,3 +372,7 @@ message.on('is-start', msg => {
 		}
 	})
 })
+
+setTimeout(() => {
+	console.log(spider.search_states);
+}, 10000);
