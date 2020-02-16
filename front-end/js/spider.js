4170004efdd09618a4c466168395d40d1ab1837b
jiandandaoxingfu <jiaminxin@outlook.com>
2020-02-16 17:18:00 +0800

    基本完成精炼数据，还有待优化。

--- front-end/js/spider.js
+++ front-end/js/spider.js
@@ -24,7 +24,7 @@ class Spider {
 		
 		this.search_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com/WOS_GeneralSearch.do?fieldCount=1&action=search&product=WOS&search_mode=GeneralSearch&SID=${this.sid}&max_field_count=25&max_field_notice=%E6%B3%A8%E6%84%8F%3A+%E6%97%A0%E6%B3%95%E6%B7%BB%E5%8A%A0%E5%8F%A6%E4%B8%80%E5%AD%97%E6%AE%B5%E3%80%82&input_invalid_notice=%E6%A3%80%E7%B4%A2%E9%94%99%E8%AF%AF%3A+%E8%AF%B7%E8%BE%93%E5%85%A5%E6%A3%80%E7%B4%A2%E8%AF%8D%E3%80%82&exp_notice=%E6%A3%80%E7%B4%A2%E9%94%99%E8%AF%AF%3A+%E4%B8%93%E5%88%A9%E6%A3%80%E7%B4%A2%E8%AF%8D%E5%8F%AF%E4%BB%A5%E5%9C%A8%E5%A4%9A%E4%B8%AA%E5%AE%B6%E6%97%8F%E4%B8%AD%E6%89%BE%E5%88%B0+%28&input_invalid_notice_limits=+%3Cbr%2F%3E%E6%B3%A8%E6%84%8F%3A+%E6%BB%9A%E5%8A%A8%E6%A1%86%E4%B8%AD%E6%98%BE%E7%A4%BA%E7%9A%84%E5%AD%97%E6%AE%B5%E5%BF%85%E9%A1%BB%E8%87%B3%E5%B0%91%E4%B8%8E%E4%B8%80%E4%B8%AA%E5%85%B6%E4%BB%96%E6%A3%80%E7%B4%A2%E5%AD%97%E6%AE%B5%E7%9B%B8%E7%BB%84%E9%85%8D%E3%80%82&sa_params=WOS%7C%7C7AVrjhmEcJpyJsy2QBT%7Chttp%3A%2F%2Fapps.webofknowledge.com%7C%27&formUpdated=true&value%28input1%29=_title_&value%28select1%29=TI&value%28hidInput1%29=&limitStatus=collapsed&ss_lemmatization=On&ss_spellchecking=Suggest&SinceLastVisit_UTC=&SinceLastVisit_DATE=&period=Range+Selection&range=ALL&startYear=1985&endYear=2020&editions=SCI&editions=SSCI&editions=AHCI&editions=ISTP&editions=ESCI&editions=CCR&editions=IC&update_back2search_link_param=yes&ssStatus=display%3Anone&ss_showsuggestions=ON&ss_numDefaultGeneralSearchFields=1&ss_query_language=&rs_sort_by=PY.D%3BLD.D%3BSO.A%3BVL.D%3BPG.A%3BAU.A`; // sid, title
 		this.cite_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+CitingArticles.do?product=WOS&SID=${this.sid}&search_mode=CitingArticles&parentProduct=WOS&parentQid=_qid_&parentDoc=1&REFID=_refid_&logEventUT=WOS:000340351500004&excludeEventConfig=ExcludeIfFromNonInterProduct&cacheurlFromRightClick=no`; // sid, qid, refid,
-		this.cite_50_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+summary.do?product=WOS&parentProduct=WOS&search_mode=CitingArticles&qid=_qid_&SID=${this.sid}&&page=1&action=changePageSize&pageSize=_size_`; // qid, sid, size=10/25/50.
+		this.cite_25_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+summary.do?product=WOS&parentProduct=WOS&search_mode=CitingArticles&qid=_qid_&SID=${this.sid}&&page=1&action=changePageSize&pageSize=25`; // qid, sid
 
 		this.detail_url = `https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+OutboundService.do?action=go&displayCitedRefs=true&displayTimesCited=true&displayUsageInfo=true&viewType=summary&product=WOS&mark_id=WOS&colName=WOS&search_mode=GeneralSearch&locale=zh_CN&view_name=WOS-summary&sortBy=PY.D%3BLD.D%3BSO.A%3BVL.D%3BPG.A%3BAU.A&mode=outputService&qid=_qid_&SID=${this.sid}&format=formatForPrint&filters=HIGHLY_CITED+HOT_PAPER+OPEN_ACCESS+PMID+USAGEIND+AUTHORSIDENTIFIERS+ACCESSION_NUM+FUNDING+SUBJECT_CATEGORY+JCR_CATEGORY+LANG+IDS+PAGEC+SABBR+CITREFC+ISSN+PUBINFO+KEYWORDS+CITTIMES+ADDRS+CONFERENCE_SPONSORS+DOCTYPE+ABSTRACT+CONFERENCE_INFO+SOURCE+TITLE+AUTHORS++&selectedIds=&mark_to=1&mark_from=1&queryNatural=%3Cb%3E%E6%A0%87%E9%A2%98%3A%3C%2Fb%3E+(_title_)&count_new_items_marked=0&MaxDataSetLimit=&use_two_ets=false&DataSetsRemaining=&IsAtMaxLimit=&IncitesEntitled=yes&value(record_select_type)=range&markFrom=1&markTo=1&fields_selection=HIGHLY_CITED+HOT_PAPER+OPEN_ACCESS+PMID+USAGEIND+AUTHORSIDENTIFIERS+ACCESSION_NUM+FUNDING+SUBJECT_CATEGORY+JCR_CATEGORY+LANG+IDS+PAGEC+SABBR+CITREFC+ISSN+PUBINFO+KEYWORDS+CITTIMES+ADDRS+CONFERENCE_SPONSORS+DOCTYPE+ABSTRACT+CONFERENCE_INFO+SOURCE+TITLE+AUTHORS++&&`; // qid, sid, title
 		this.journal_info_url = `http://www.fenqubiao.com/Core/JournalDetail.aspx?y=2019&t=`; // journal.
@@ -68,73 +68,74 @@ class Spider {
 				this.search_states[id][4] = 1;
 			}
 
-			console.log( `${id + 1} :${info}` + new Date().getMinutes() + ':' + new Date().getSeconds() );
+			console.log( `${id + 1} :已经完成搜索-${info}` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		})
 	}
 
-	get_cite_data(id) {
+	async get_cite_data(id) {
 		console.log(id + 1 + ' : ' + '正在获取引用文献' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.search_states[id][1] = 1;
 		let cite_url = this.cite_url.replace('_qid_', this.qid_arr[id])
 			.replace('_refid_', this.refid_arr[id]);
-		return axios.get(cite_url).then( res => {
-			let data = res.data.replace(/(\r\n|\r|\n)/g, '');
-			let record_num = data.match(/id="RECORD_\d+/g).length; // 0, <=10, <=25, <=50
-			console.log(record_num);
-			if( record_num > 0 ) {
-				// 判断引用文献中是否含有所需年份内发表的，如果有，计算出最大引用量：即所需最小年份以后的引用量,从而确定爬取页数。
-				let year_and_num = data.match(/\d{4}\s\(\d+\)<\/label>/g);
-				let year_arr = year_and_num.map( s => parseInt( s.slice(0, 4) ) );
-				let num_arr = year_and_num.map( s => parseInt( s.slice(6, 8) ));
-				let intersect = this.year_arr.filter( y => year_arr.includes(y) );
-				if( intersect.length > 0 ) {
-					let min_intersect_year = Math.min(...intersect);
-					let index = year_arr.indexOf(min_intersect_year);
-					let sum = num_arr.slice(0, index + 1).reduce( (i, j) => i+j );
-					data = data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/);
-					data = 
-					return [record_num, num, data];
-				} else {
-					this.search_states[id][1] = 2;
-					this.search_states[id][4] = 1;
-				}
+		let res = await axios.get(cite_url);
+		let data = res.data.replace(/(\r\n|\r|\n)/g, '');
+		let record_num = data.match(/id="RECORD_\d+/g).length; // 0, <=10, <=25
+		console.log(id + 1 + '：引用页数据量为' + record_num);
+		if( record_num > 0 ) {
+			// 判断引用文献中是否含有所需年份内发表的，如果有，计算出最大引用量：即所需最小年份以后的引用量,从而确定爬取页数。
+			let year_and_num = data.match(/\d{4}\s\(\d+\)<\/label>/g);
+			let year_arr = year_and_num.map( s => parseInt( s.slice(0, 4) ) );
+			let num_arr = year_and_num.map( s => parseInt( s.slice(6, 8) ));
+			let intersect = this.year_arr.filter( y => year_arr.includes(y) );
+			if( intersect.length > 0 ) {
+				let min_intersect_year = Math.min(...intersect);
+				let index = year_arr.indexOf(min_intersect_year);
+				let sum = num_arr.slice(0, index + 1).reduce( (i, j) => i+j );
+				return [record_num, sum, data];
 			} else {
-				this.search_states[id][1] = -1;
-				this.search_states[id][4] = 1;
+				this.search_states[id][1] = 2;
 			}
-			return '';
-		})
+		} else {
+			this.search_states[id][1] = -1;
+		}
+		this.search_states[id][4] = 1;
+		return '';
 	}
 
-	async get_cite_refine_data(record_num, sum, data) {
-		if( record_num >= sum ) {
-			data = data.reduce( (i, j) => i+j );
-		} else {
-			data = await this.get_more_cite_data();
+	async next_cite_page(data) {
+		let next_page_url = data.match(/class="paginationNext.*?href="(.*?)"/)[1].replace(/amp;/g, '');
+		let res = await axios.get(next_page_url);	
+		data = res.data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/g);
+		return [data.reduce( (i, j) => i+j ), res.data];
+	}
+
+	async get_cite_refine_data(record_num, sum, res_data, id) {
+		console.log(id + 1 + ' : ' + '正在精炼文献' + new Date().getMinutes() + ':' + new Date().getSeconds() );
+		let data = res_data.match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/g);
+		data = data.reduce( (i, j) => i+j );
+		let data_ = '';
+		if( record_num < sum ) {
+			if( record_num == 10 && sum <= 20 ) { // 下一页，
+				data += await this.next_cite_page(res_data)[0];
+			} else if( (record_num == 10 && sum > 20) || record_num == 25 ) { 
+				if( record_num == 10 ) { // 先调整每页显示25个，
+					let cite_25_url = `class="paginationNext href="${this.cite_25_url.replace('_qid_', this.qid_arr[id] + 1)}"`;
+					[data_, res_data] = await this.next_cite_page(cite_25_url);
+					data += data_;
+				}
+				let pages = Math.floor(sum/25);
+				for( let i=0; i<pages; i++ ) {
+					[data_, res_data] = await this.next_cite_page(res_data);
+					data += data_;
+				}
+			}
 		}
+
 		data = this.search_result_format(data);
 		let ele = this.refine_cite_data(data);
 		[this.cite_refine_datas[id], this.cite_num_arr[id]] = this.get_cite_num(ele);
-	}
-	async get_more_cite_data(data, sum) {
-		if( sum < 26 && sum > 11 ) {
-			let cite_50_url = this.cite_50_url.replace('_qid_', this.qid_arr[id] + 1)
-					.replace('_size_', 25);
-			let res = await axios.get(cite_50_url);
-			data += res.data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/g)[1];
-		} else {
-			let pages = Math.floor(sum/50);
-			let cite_50_url = this.cite_50_url.replace('_qid_', this.qid_arr[id] + 1)
-					.replace('_size_', 50);
-			res = await axios.get(cite_50_url);
-			data += res.data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/g)[1];
-			for(let i=0; i<pages; i++) {
-				let next_page_url = res.data.match(/class="paginationNext.*?href="(.*?)"/)[1].replace(/amp;/g, '');
-				res = await axios.get(next_page_url);
-				let matches = res.data.replace(/(\r\n|\r|\n)/g, '').match(/<div class="search-results">.*?name="LinksAreAllowedRightClick" value="CitedPatent\.do"/g);
-				data += matches[0] + matches[1];
-			}
-		}
+		this.search_states[id][1] = 2;
+		console.log(id + 1 + ' : ' + '已经完成精炼数据' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 	}
 
 	refine_cite_data(data) {
@@ -176,36 +177,34 @@ class Spider {
 		return [body.innerHTML, [other_cite_num, self_cite_num]];
 	}
 
-	get_detail_data(id) {
-		console.log(id + 1 + ' : ' + '抓取详情页' + new Date().getMinutes() + ':' + new Date().getSeconds() );
+	async get_detail_data(id) {
+		console.log(id + 1 + ' : ' + '正在抓取详情页' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.search_states[id][2] = 1;
 		let detail_url = this.detail_url.replace('_qid_', this.qid_arr[id])
 			.replace('_title_', this.title_arr[id]);
-		return axios.get(detail_url).then( res => {
-			if( true ) { // 判断是否成功。
-				this.search_states[id][2] = 2;
-				this.detail_tables[id] = this.detail_table_format(res.data);
-			} else {
-				this.search_states[id][2] = -1;
-			}
-			console.log(id + 1 + ' : ' + '已经完成详情页处理' + new Date().getMinutes() + ':' + new Date().getSeconds() );
-		})
+		let res = await axios.get(detail_url);
+		if( true ) { // 判断是否成功。
+			this.search_states[id][2] = 2;
+			this.detail_tables[id] = this.detail_table_format(res.data);
+		} else {
+			this.search_states[id][2] = -1;
+		}
+		console.log(id + 1 + ' : ' + '已经完成详情页处理' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 	}
 
-	get_journal_data(id) { 
+	async get_journal_data(id) { 
 		console.log(id + 1 + ' : ' + '正在获取期刊分区数据' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		this.search_states[id][3] = 1;
 		let journal_info_url = this.journal_info_url + this.journal_arr[id];
-		return axios.get(journal_info_url).then( res => {
-			let data = this.journal_table_format(res.data);
-			if( data !== '' ) {
-				this.search_states[id][3] = 2;
-				this.journal_tables[id] = data;
-			} else {
-				this.search_states[id][3] = -1;
-			}
-			console.log(id + 1 + ' : ' + '已经完成期刊分区处理' + new Date().getMinutes() + ':' + new Date().getSeconds() );
-		})
+		let res = await axios.get(journal_info_url);
+		let data = this.journal_table_format(res.data);
+		if( data !== '' ) {
+			this.search_states[id][3] = 2;
+			this.journal_tables[id] = data;
+		} else {
+			this.search_states[id][3] = -1;
+		}
+		console.log(id + 1 + ' : ' + '已经完成期刊分区处理' + new Date().getMinutes() + ':' + new Date().getSeconds() );
 	}
 
 	search_result_format(data) {
@@ -279,23 +278,23 @@ class Spider {
 	}
 
 	async crawl(id) {
-		console.log(`${id + 1} : 个开始运行` + new Date().getMinutes() + ':' + new Date().getSeconds() );
+		console.log(`${id + 1} : 开始运行` + new Date().getMinutes() + ':' + new Date().getSeconds() );
 		await this.get_search_data(id);
 		if( this.search_states[id][0] == 2 && this.search_states[id][4] == 0 ) {
-			this.get_cite_data(id).then( res => {
-				if( res !== '' ) {
-					await Promise.all[
-						this.get_detail_data(id);
-						this.get_journal_data(id);
-						this.get_cite_refine_data(res);
-					];
-					this.render(id);
-					this.next();
-				} else {
-					this.render(id);
-					this.next();
-				}
-			});
+			let res = await this.get_cite_data(id);
+			if( res !== '' ) {
+				await Promise.all([
+					this.get_detail_data(id),
+					this.get_journal_data(id),
+					this.get_cite_refine_data(...res, id)
+				]);
+				this.search_states[id][4] = 1;
+				this.render(id);
+				this.next();
+			} else {
+				this.render(id);
+				this.next();
+			}
 		} else if( this.search_states[id][4] == 1 ) {
 			this.render(id);
 			this.next();
@@ -366,22 +365,3 @@ class Spider {
 		message.send(this.after_end_tad_id, 'done', {});
 	}
 }
-
-
-function get_data() {
-	// return axios.get('https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com/WOS_GeneralSearch.do?fieldCount=1&action=search&product=WOS&search_mode=GeneralSearch&SID=6EuUympOfp3mZydOGqr&max_field_count=25&max_field_notice=%E6%B3%A8%E6%84%8F%3A+%E6%97%A0%E6%B3%95%E6%B7%BB%E5%8A%A0%E5%8F%A6%E4%B8%80%E5%AD%97%E6%AE%B5%E3%80%82&input_invalid_notice=%E6%A3%80%E7%B4%A2%E9%94%99%E8%AF%AF%3A+%E8%AF%B7%E8%BE%93%E5%85%A5%E6%A3%80%E7%B4%A2%E8%AF%8D%E3%80%82&exp_notice=%E6%A3%80%E7%B4%A2%E9%94%99%E8%AF%AF%3A+%E4%B8%93%E5%88%A9%E6%A3%80%E7%B4%A2%E8%AF%8D%E5%8F%AF%E4%BB%A5%E5%9C%A8%E5%A4%9A%E4%B8%AA%E5%AE%B6%E6%97%8F%E4%B8%AD%E6%89%BE%E5%88%B0+%28&input_invalid_notice_limits=+%3Cbr%2F%3E%E6%B3%A8%E6%84%8F%3A+%E6%BB%9A%E5%8A%A8%E6%A1%86%E4%B8%AD%E6%98%BE%E7%A4%BA%E7%9A%84%E5%AD%97%E6%AE%B5%E5%BF%85%E9%A1%BB%E8%87%B3%E5%B0%91%E4%B8%8E%E4%B8%80%E4%B8%AA%E5%85%B6%E4%BB%96%E6%A3%80%E7%B4%A2%E5%AD%97%E6%AE%B5%E7%9B%B8%E7%BB%84%E9%85%8D%E3%80%82&sa_params=WOS%7C%7C7AVrjhmEcJpyJsy2QBT%7Chttp%3A%2F%2Fapps.webofknowledge.com%7C%27&formUpdated=true&value%28input1%29=Riemann-Hilbert approach and N-soliton solutions for a generalized Sasa-Satsuma equation&value%28select1%29=TI&value%28hidInput1%29=&limitStatus=collapsed&ss_lemmatization=On&ss_spellchecking=Suggest&SinceLastVisit_UTC=&SinceLastVisit_DATE=&period=Range+Selection&range=ALL&startYear=1985&endYear=2020&editions=SCI&editions=SSCI&editions=AHCI&editions=ISTP&editions=ESCI&editions=CCR&editions=IC&update_back2search_link_param=yes&ssStatus=display%3Anone&ss_showsuggestions=ON&ss_numDefaultGeneralSearchFields=1&ss_query_language=&rs_sort_by=PY.D%3BLD.D%3BSO.A%3BVL.D%3BPG.A%3BAU.A').then( res => {
-		// let qid = parseInt(res.data.match(/qid=(\d+)/)[1]);
-		// let refid = res.data.match(/REFID=(\d+)/)[1];
-		axios.get(`https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+CitingArticles.do?product=WOS&SID=8DmR2x785pYk4aAaSBZ&search_mode=CitingArticles&parentProduct=WOS&parentQid=2&parentDoc=2&REFID=469852762&logEventUT=WOS:000334404800007&excludeEventConfig=ExcludeIfFromNonInterProduct&cacheurlFromRightClick=no`).then( res => {
-			axios.get(`https://vpn2.zzu.edu.cn/,DanaInfo=apps.webofknowledge.com+summary.do?product=WOS&parentProduct=WOS&search_mode=CitingArticles&qid=4&SID=8DmR2x785pYk4aAaSBZ&&page=1&action=changePageSize&pageSize=50`).then( res => {
-				let next_page_url = res.data.match(/class="paginationNext.*?href="(.*?)"/)[1].replace(/amp;/g, '');
-				axios.get(next_page_url).then( res => {
-					console.log(res);
-				})
-
-			})	
-		})
-	// })
-}
-
-// get_data();
